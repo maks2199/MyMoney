eb12f17ff26cebb22c57259c455150a12537149b
maks2199 <maks21992199@mail.ru>
2024-03-30 23:25:15 +0300

    initial commit

--- public/client.js
+++ public/client.js
@@ -1,53 +1,104 @@
-var slider1 = document.getElementById("data1");
-var slider2 = document.getElementById("data2");
-var slider3 = document.getElementById("data3");
-
-var output1 = document.getElementById("demo1");
-var output2 = document.getElementById("demo2");
-var output3 = document.getElementById("demo3");
-output1.innerHTML = slider1.value; // Display the default slider value
-output2.innerHTML = slider2.value; 
-output3.innerHTML = slider3.value; 
 
 
+slidersConfig = [
+    {
+        "name": "Еда",
+        "startValue": 10000
+    },
+    {
+        "name": "Транспорт",
+        "startValue": 5000
+    },
+    {
+        "name": "Связь",
+        "startValue": 3000
+    },
+    {
+        "name": "Кофе",
+        "startValue": 2000
+    },
+    {
+        "name": "Аренда",
+        "startValue": 30000
+    }
+]
+
+// Creating sliders
+const sliderContainer = document.querySelector('.slidecontainer')
+slidersConfig.forEach((slider) =>
+{
+    newSlider = document.createElement('input')
+    newSlider.type = 'range'
+    newSlider.min = 0
+    newSlider.max = 100000
+    newSlider.step = 1000
+    newSlider.value = slider.startValue
+    newSlider.classList.add('slider')
+
+    newSliderP = document.createElement('p')
+    newSliderP.textContent = slider.name + ": "
+    newSliderSpan = document.createElement('span')
+    newSliderSpan.classList.add('sliderValue')
+    newSliderSpan.textContent = slider.startValue
+    newSliderP.appendChild(newSliderSpan)
+
+    newSliderDiv = document.createElement('div')
+    newSliderDiv.classList.add('slider')
+    newSliderDiv.appendChild(newSlider)
+    newSliderDiv.appendChild(newSliderP)
+
+    sliderContainer.appendChild(newSliderDiv)
+})
 
 
 
+
+const autocolors = window['chartjs-plugin-autocolors'];
+// Chart
 const ctx = document.getElementById('myChart');
 var chart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Data1', 'Data2', 'Data3'],
+        labels: slidersConfig.map(slider => slider.name),
         datasets: [{
-            data: [slider1.value, slider2.value, slider3.value],
-            borderWidth: 5
+            data: slidersConfig.map(slider => slider.startValue),
+            borderWidth: 5,
         }]
     },
     options: {
+        plugins: {
+            autocolors: {
+                mode: 'data'
+            }
+        },
         scales: {
             y: {
                 beginAtZero: true
             }
-        }
+        },
+        animationSteps: 60,
+        indexAxis: 'y',
     }
 });
+Chart.register(autocolors);
 
-// Update the current slider value (each time you drag the slider handle)
-slider1.oninput = function () {
-    output1.innerHTML = this.value;
 
-    console.log(chart.data.datasets[0].data[0] = slider1.value)
+
+// Sliders events
+sliders = document.querySelectorAll('input.slider')
+for (let i = 0; i < sliders.length; i++){
+    sliders[i].addEventListener('input', () => {
+    siblingP = sliders[i].nextElementSibling
+    sliderSpan = siblingP.querySelector('.sliderValue')
+    sliderSpan.textContent = sliders[i].value
+
+    // Update bar chart
+    chart.data.datasets[0].data[i] = sliders[i].value
     chart.update()
+    })
 }
-slider2.oninput = function () {
-    output2.innerHTML = this.value;
 
-    console.log(chart.data.datasets[0].data[1] = slider2.value)
-    chart.update()
-}
-slider3.oninput = function () {
-    output3.innerHTML = this.value;
 
-    console.log(chart.data.datasets[0].data[2] = slider3.value)
-    chart.update()
-}
+
+
+
