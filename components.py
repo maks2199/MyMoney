f87eec9ebab5e97cab6eac2afac9b0dd098cf489
Tachkov.Maksim <tachkov.maksim@gmail.com>
2024-08-10 20:02:51 +0300

    every month of year graph

---
+++ components.py
@@ -0,0 +1,197 @@
+import pandas as pd
+from datetime import datetime, timedelta
+import streamlit as st
+import plotly.express as px
+import json
+
+def get_current_month_interval():
+    now = datetime.now()
+    month_start = (now.replace(day=1, hour=0, minute=0, second=0, microsecond=0)).date()
+    nxt_mnth = now.replace(day=28) + timedelta(days=4)
+    month_end = (nxt_mnth - timedelta(days=nxt_mnth.day)).date()
+
+    return month_start, month_end
+
+def get_month_interval(date):
+    month_start = (date.replace(day=1))
+    nxt_mnth = date.replace(day=28) + timedelta(days=4)
+    month_end = (nxt_mnth - timedelta(days=nxt_mnth.day))
+
+    return month_start, month_end
+
+def get_current_month_df(df: pd.DataFrame) -> pd.DataFrame:
+    now = datetime.now()
+    month_start = now.replace(day=1, hour=0, minute=0, second=0, microsecond=0).date()
+    df_month = df[df['date'] >= month_start]
+
+    return df_month
+
+def slice_df_by_time(df: pd.DataFrame, time_min, time_max) -> pd.DataFrame:
+    
+    df_month = df[(df['date'] >= pd.to_datetime(time_min).date()) & (df['date'] <= pd.to_datetime(time_max).date())]
+
+    return df_month
+
+# def month_graph(df: pd.DataFrame) -> None:
+
+#     df = df[df['sup_cat'] != 'transaction']
+
+#     # Get only current month
+#     current_month_start_date, current_month_end_date = get_current_month_interval()
+#     df_month = slice_df_by_time(df, current_month_start_date, current_month_end_date)
+
+#     # Grouping
+#     df_month_grouped = df_month.groupby(['sup_cat'])['amount'].sum()
+#     total_sum = df_month_grouped.sum()
+#     df_month_grouped = df_month_grouped.to_frame()
+#     df_month_grouped = df_month_grouped.rename(columns={'amount': 'b-month'})
+#     print(df_month_grouped)
+
+
+
+
+#     now = datetime.now()
+#     week_start = now + timedelta(days = -now.weekday())
+#     week_start = week_start.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    
+
+#     df_week = df[df['date'] >= week_start]
+#     print(df_week.tail(20))
+#     df_week_grouped = df_week.groupby(['sup_cat'])['amount'].sum()
+#     df_week_grouped = df_week_grouped.to_frame()
+#     df_week_grouped = df_week_grouped.rename(columns={'amount': 'a-week'})
+#     print(df_week_grouped)
+
+#     # Merge
+#     df_merged = df_month_grouped.merge(df_week_grouped,  left_index=True, right_index=True)
+#     print(df_merged)
+
+
+#     # Visualize
+#     with st.container(border=True):
+#         st.write('# This month expenses')
+#         st.bar_chart(df_merged)
+#         st.markdown(f'## {"{:,}".format(round(total_sum)).replace(',',' ')} ₽')
+
+
+if __name__ == '__main__':
+    now = datetime.now()
+    week_start = now + timedelta(days = -now.weekday())
+    week_start = week_start.replace(hour=0, minute=0, second=0, microsecond=0)
+    print(week_start)
+
+def current_month_category_graph(df: pd.DataFrame):
+    current_month_start_date, current_month_end_date = get_current_month_interval()
+    month_category_graph(df, current_month_start_date, current_month_end_date)
+
+def choosed_month_category_graph(df, start_date):
+    month_start, month_end = get_month_interval(start_date)
+    month_category_graph(df, month_start, month_end)
+
+def month_category_graph(df, month_start_date, month_end_date):
+    df = df[df['sup_cat'] != 'transaction']
+
+    # # Get only current month
+    # current_month_start_date, current_month_end_date = get_current_month_interval()
+    df_month = slice_df_by_time(df, month_start_date, month_end_date)
+
+    
+    # Pivoting
+    df_month_grouped = df_month[['date','amount','sup_cat']].groupby(by=['sup_cat','date']).sum()
+    df_month_grouped = df_month_grouped.reset_index()
+    total_sum = df_month_grouped['amount'].sum()
+    df_month_pivoted = df_month_grouped.pivot(index='date', columns='sup_cat', values='amount')
+    df_month_pivoted = df_month_pivoted.fillna(0)
+    
+
+    # Cumsum
+    df_summed = df_month_pivoted.cumsum()
+    print('---Month summary table---')
+    print(df_summed)
+
+
+
+    # Visualize
+    fig_fact = px.line(df_summed)
+
+    # Planned lines
+    with open('configs/plannedExpenses.json') as file:
+        planned_expenses = json.load(file)
+    plan_table = {'date': [month_start_date, month_end_date]}
+    for key, value in planned_expenses.items():
+        plan_table[key] = [0, value]
+    plan = pd.DataFrame(plan_table)
+    plan = plan.set_index(['date'])
+    print('---Planned table---')
+    print(plan)
+
+
+    fig_plan = px.line(plan)
+    fig_fact.add_traces(fig_plan.data)
+    fig_fact.update_traces(patch={"line": {"dash": "dot"}}, selector=lambda x: True if 'p_' in x.name else False)
+    fig_fact.update_traces(line=dict(width=5.0))
+
+
+    with st.container(border=True):
+        st.write(f'# :blue[{month_start_date.strftime("%B")}] expenses: {"{:,}".format(round(total_sum)).replace(',',' ')} ₽')
+        st.plotly_chart(fig_fact, use_container_width=True)
+
+
+# def category_graph_by_time(df, time_min, time_max):
+#     print('catgory graph')
+
+#     df = df[df['sup_cat'] != 'transaction']
+
+#     # Get only current month
+#     df_month = slice_df_by_time(df, time_min, time_max)
+
+   
+
+#     # Pivoting
+#     df_month_grouped = df_month[['date','amount','sup_cat']].groupby(by=['sup_cat','date']).sum()
+#     df_month_grouped = df_month_grouped.reset_index()
+#     print(df_month_grouped)
+#     df_month_pivoted = df_month_grouped.pivot(index='date', columns='sup_cat', values='amount')
+#     df_month_pivoted = df_month_pivoted.fillna(0)
+#     print(df_month_pivoted)
+
+#     # Cumsum
+#     df_summed = df_month_pivoted.cumsum()
+#     print(df_summed)
+
+
+
+#     # Visualize
+#     fig = px.line(df_summed)
+
+#     ## Month's first and last days
+
+
+#     plan = pd.DataFrame({
+#     'date': [time_min, time_max],
+#     'caffe_p': [0, 20000],
+#     'groceries_p': [0, 30000],
+#     'other_p': [0, 4000],
+#     'transport_p': [0, 6000],
+#     })
+
+#     plan = plan.set_index(['date'])
+#     print('plan')
+#     print(plan)
+
+
+#     fig2 = px.line(plan)
+
+#     # for d in fig2.data:
+#         # fig.add_trace(d)
+#     fig.add_traces(fig2.data)
+#     # fig.add_scatter(x=plan['date'], y=plan['caffe'], mode='lines')
+#     # fig.update_layout(title='Stock vs Prediction', xaxis_title='Date', yaxis_title='Value')
+#     fig.update_traces(patch={"line": {"dash": "dot"}}, selector=lambda x: True if '_p' in x.name else False)
+#     fig.update_traces(line=dict(width=5.0))
+
+
+
+#     st.plotly_chart(fig, use_container_width=True)
+
--- main.py
+++ main.py
@@ -3,26 +3,27 @@ import altair as alt
 import pandas as pd
 import math
 from datetime import datetime
+import json
 
-import graphs
+import components
 
+st.set_page_config(layout="wide")
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 st.write("https://www.tbank.ru/mybank/")
-
 file = st.file_uploader('Excel')
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 df_raw = pd.read_excel(file)
-
-# Write to DataBase new raws
-
+print('---Raw data table---')
 print(df_raw)
 print(df_raw.info())
-
 df = df_raw.copy()
 
 # Filter only success expensess
 df = df[df['Статус'] == 'OK']
 df = df[df['Сумма операции'] < 0]
 df = df.reset_index()
+# Preparing table
 df = df[['Дата операции', 'Статус', 'Сумма операции', 'Категория', 'Описание']]
 df = df.rename(columns={
     'Дата операции': 'date', 
@@ -32,52 +33,57 @@ df = df.rename(columns={
     'Описание': 'description'
 })
 df['date'] = df['date'].map(lambda d: datetime.strptime(d, '%d.%m.%Y %H:%M:%S'))
+df['date'] = df['date'].map(lambda d: d.date())
 df['amount'] = df['amount'] * -1
 
 
 # Mapping categories
-super_categories_map = {
-    'Такси': 'transport',
-    'Местный транспорт': 'transport',
-    'Ж/д билеты': 'transport',
-    'Транспорт': 'transport',
-    'Супермаркеты': 'groceries',
-    'Фастфуд': 'caffe',
-    'Рестораны': 'caffe',
-    'Caffe': 'caffe',
-    'Переводы': 'transaction',
-    'Наличные': 'transaction'
-}
+with open('configs/mapCategories.json') as file:
+    super_categories_map = json.load(file)
 df['sup_cat'] = df['category'].map(super_categories_map)
 df['sup_cat'].fillna('other', inplace=True)
+print('---Mapped categories---')
 print(df.tail(10))
 
-st.dataframe(df)
-
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+st.dataframe(df, use_container_width=True)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Grouping
 df_grouped = df.groupby(['sup_cat'])['amount'].sum()
+print('---Groupped data---')
 print(df_grouped)
 
 
 
-# # Visualization
-# df_vizualize = df_grouped.copy()
-# df_vizualize = df_vizualize.drop('transaction')
-# df_vizualize = df_vizualize 
-# st.dataframe(df_vizualize)
-# st.bar_chart(df_vizualize)
-
-
-
-
 # Month graph
-graphs.month_graph(df)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# components.month_graph(df)
+components.current_month_category_graph(df)
+time_min = st.date_input("Date range", value=(datetime.now()))
+components.choosed_month_category_graph(df, time_min)
 
-graphs.category_graph(df)
+
+tiles = []
+row1 = st.columns(3)
+row2 = st.columns(3)
+row3 = st.columns(3)
+row4 = st.columns(3)
+tiles.extend(row1 + row2 + row3 + row4)
 
 
-time_min, time_max = st.date_input("Date range", value=(datetime.now(), datetime.now()))
+
+dates= pd.date_range('2024-01-01','2025-01-01' , freq='1M')-pd.offsets.MonthBegin(1)
+
+# for col in row1 + row2 + row3 + row4:
+#     with col.container(height=120):
+#         st.write('hi')
+
+for i in range(len(tiles)):
+    with tiles[i].container():
+        components.choosed_month_category_graph(df, dates[i])
 
 
-graphs.category_graph_by_time(df, time_min, time_max)
+# for date in dates:
+#     components.choosed_month_category_graph(df, date)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
